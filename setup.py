11a70f968abde10722b27e1541094b38560a78ee
nima.dehkordi <nima.dehkordi@gmx.de>
2023-01-03 15:30:03 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is the solution to https://www.kaggle.com/c/otto-recommender-system challenge ',
+    author='Nima Dehkordi',
+    license='',
+)
